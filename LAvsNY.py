d7cf37e98de96dd18f88d780309702f6c176b80a
andrewchabot <afc1755@rit.edu>
2019-04-09 22:47:02 -0400

    Code got changed, I think it works now

--- LAvsNY.py
+++ LAvsNY.py
@@ -10,21 +10,21 @@ from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
 
-def LAvsNY(NYFold, LAFold):
+def LAvsNY():
     classifier = Sequential()
     classifier.add(Conv2D(32, (3,3), input_shape=(64,64,3), activation='relu'))
     classifier.add(MaxPooling2D(pool_size = (2,2)))
     classifier.add(Flatten())
     classifier.add(Dense(units = 128, activation='relu'))
-    classifier.add(Dense(units=1, activation='sigmoid'))
-    classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    classifier.add(Dense(units=4, activation='sigmoid'))
+    classifier.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
     train_datagen = ImageDataGenerator(rescale = 1./255, shear_range = 0.2, horizontal_flip = True)
     test_datagen = ImageDataGenerator(rescale = 1./255)
-    training_set = train_datagen.flow_from_directory('training_set', target_size=(64,64),batch_size=32, class_mode='binary')
-    test_set = test_datagen.flow_from_directory('test_set', target_size = (64, 64), batch_size = 32, class_mode = 'binary')
+    training_set = train_datagen.flow_from_directory('training_set', target_size=(64,64),batch_size=32, class_mode='categorical')
+    test_set = test_datagen.flow_from_directory('test_set', target_size = (64, 64), batch_size = 32, class_mode = 'categorical')
 
-    classifier.fit_generator(training_set, steps_per_epoch=295, epochs=300, validation_data=test_set, validation_steps=67)
+    classifier.fit_generator(training_set, steps_per_epoch=511, epochs=25, validation_data=test_set, validation_steps=149)
 
     for img in os.listdir("test"):
         test_image = image.load_img('test/' + img, target_size=(64, 64))
@@ -38,8 +38,4 @@ def LAvsNY(NYFold, LAFold):
             prediction = 'LA'
         print(img + " :" + prediction)
 
-
-LAFolder = "LA"
-NYFolder = "NY"
-
-LAvsNY(LAFolder, NYFolder)
+LAvsNY()
