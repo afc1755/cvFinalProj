0bf20a6788883587f255c27f3f41933fe8223511
andrewchabot <afc1755@rit.edu>
2019-04-10 14:30:48 -0400

    This actual learns now, fixed some important things like loss function, added some layers

--- LAvsNY.py
+++ LAvsNY.py
@@ -9,20 +9,27 @@ from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import LeakyReLU
 
 def LAvsNY():
     classifier = Sequential()
     classifier.add(Conv2D(32, (3,3), input_shape=(64,64,3), activation='relu'))
     classifier.add(MaxPooling2D(pool_size = (2,2)))
     classifier.add(Flatten())
-    classifier.add(Dense(units = 128, activation='relu'))
-    classifier.add(Dense(units=4, activation='sigmoid'))
+    classifier.add(Dense(100, activation=LeakyReLU(alpha=0.3)))
+    classifier.add(Dropout(0.5))
+    classifier.add(Dense(50, activation=LeakyReLU(alpha=0.3)))
+    classifier.add(Dropout(0.3))
+    classifier.add(Dense(units=4, activation='softmax'))
     classifier.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
     train_datagen = ImageDataGenerator(rescale = 1./255, shear_range = 0.2, horizontal_flip = True)
     test_datagen = ImageDataGenerator(rescale = 1./255)
-    training_set = train_datagen.flow_from_directory('training_set', target_size=(64,64),batch_size=32, class_mode='categorical')
-    test_set = test_datagen.flow_from_directory('test_set', target_size = (64, 64), batch_size = 32, class_mode = 'categorical')
+    training_set = train_datagen.flow_from_directory('training_set', target_size=(64,64),batch_size=32,\
+                                                     class_mode='categorical')
+    test_set = test_datagen.flow_from_directory('test_set', target_size=(64, 64), batch_size=32,\
+                                                class_mode='categorical')
 
     classifier.fit_generator(training_set, steps_per_epoch=511, epochs=25, validation_data=test_set, validation_steps=149)
 
