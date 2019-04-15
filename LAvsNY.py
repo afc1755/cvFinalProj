783b355e2c9619a31b0ac2b029ad3e3b5be4c3e0
andrewchabot <afc1755@rit.edu>
2019-04-15 10:39:59 -0400

    should enable saving our model so that it can be demoed

--- LAvsNY.py
+++ LAvsNY.py
@@ -2,6 +2,7 @@
 #Computer Vision Final Project Proof of Concept
 import os
 import numpy as np
+from keras.engine.saving import model_from_json
 from keras.preprocessing import image
 from keras_preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
@@ -17,9 +18,9 @@ def LAvsNY():
     classifier.add(Conv2D(32, (3,3), input_shape=(64,64,3), activation='relu'))
     classifier.add(MaxPooling2D(pool_size = (2,2)))
     classifier.add(Flatten())
-    classifier.add(Dense(100, activation=LeakyReLU(alpha=0.3)))
+    classifier.add(Dense(10, activation=LeakyReLU(alpha=0.3)))
     classifier.add(Dropout(0.5))
-    classifier.add(Dense(50, activation=LeakyReLU(alpha=0.3)))
+    classifier.add(Dense(5, activation=LeakyReLU(alpha=0.3)))
     classifier.add(Dropout(0.3))
     classifier.add(Dense(units=4, activation='softmax'))
     classifier.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
@@ -31,18 +32,33 @@ def LAvsNY():
     test_set = test_datagen.flow_from_directory('test_set', target_size=(64, 64), batch_size=32,\
                                                 class_mode='categorical')
 
-    classifier.fit_generator(training_set, steps_per_epoch=511, epochs=25, validation_data=test_set, validation_steps=149)
+    classifier.fit_generator(training_set, steps_per_epoch=512, epochs=2, validation_data=test_set, validation_steps=149)
 
+    model_json = classifier.to_json()
+    with open("model.json", "w") as json_file:
+        json_file.write(model_json)
+    # serialize weights to HDF5
+    classifier.save_weights("model.h5")
+    print("Saved model to disk")
+
+def loadModel():
+    json_file = open('model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights("model.h5")
+    print("Loaded model from disk")
+
+    # evaluate loaded model on test data
+    loaded_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     for img in os.listdir("test"):
         test_image = image.load_img('test/' + img, target_size=(64, 64))
         test_image = image.img_to_array(test_image)
         test_image = np.expand_dims(test_image, axis=0)
-        result = classifier.predict(test_image)
-        training_set.class_indices
-        if result[0][0] == 1:
-            prediction = 'NY'
-        else:
-            prediction = 'LA'
-        print(img + " :" + prediction)
+        result = loaded_model.predict(test_image)
+        print(img + " :" + result)
 
 LAvsNY()
+
+loadModel()
